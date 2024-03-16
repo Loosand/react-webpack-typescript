42311c0dbddc6bf8220b14210f47a08f103fa486
Loosand <loosand@163.com>
2024-03-17 06:01:32 +0800

    STEP 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import "./style.css"
+import IMAGE from "./avatar.png"
+import LOGO from "./react.svg"
+
 export default function App() {
-	return <h1>Hello World</h1>
+	return (
+		<>
+			<h1>Hello World</h1>
+			<img src={IMAGE} alt="avatar" width="300" height="300" />
+			<img src={LOGO} alt="logo" width="100" height="100" />
+		</>
+	)
 }
